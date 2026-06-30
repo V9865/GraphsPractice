3ba67bd037e206a3482b571b33509bda12d791bd
V9865 <varshith.tadiparthi@gmail.com>
2026-06-30 15:12:04 +0530

    Time: 4 ms (30.31%) | Memory: 100.6 MB (35.54%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edge) {
-        if(edge.size()==1){
-            return 1;
-        }
         vector<int> Degree((edge.size()+2), 0);
         for(int i=0; i<edge.size(); i++){
             Degree[edge[i][0]]++;
